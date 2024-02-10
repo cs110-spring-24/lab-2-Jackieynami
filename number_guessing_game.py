0752c8768cfd06f61ad6458ab6cf30e059a350c4
Jackieynami <157658018+Jackieynami@users.noreply.github.com>
2024-02-10 02:35:14 -0800

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -10,3 +10,28 @@ elif user < num:
     print("Too low, it was", num)
 else:
     print("You got it!")
+
+user = 0
+while user != num:
+    user = int(input(f" Guess a number between {low} and {high}: "))
+    
+    if user > num:
+        print("Too high!")
+        guess_count += 1
+        high = user
+    elif user < num:
+        print("Too low!")
+        guess_count += 1
+        low = user
+    else:
+        print("You win!")
+        guess_count += 1
+        print(f"You guess {guess_count} times!")
+
+startover = input("Play again? ")
+if startover == "Yes":
+    print("Goodbye!")
+
+
+    
+    
